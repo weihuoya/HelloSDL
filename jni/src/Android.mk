a940aaccda1610ff544f2034a39ad317ac79793c
zhang wei <zwdreams@gmail.com>
2014-07-02 16:32:54 +0800

    add sigc++

--- jni/src/Android.mk
+++ jni/src/Android.mk
@@ -6,12 +6,15 @@ LOCAL_MODULE := main
 
 SDL_PATH := ../SDL
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/$(SDL_PATH)/include \
     ../SDL2_image \
-    ../SDL2_ttf
+    ../SDL2_ttf \
+    ../sigcpp2
 
 # Add your application source files here...
-LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
+LOCAL_SRC_FILES := \
+    $(SDL_PATH)/src/main/android/SDL_android_main.c \
     main.cpp \
     eventhandler.cpp \
     glcontext.cpp \
@@ -20,7 +23,7 @@ LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
     matrix.cpp \
     utool.cpp
 
-LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_ttf
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_ttf sigc++
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 
